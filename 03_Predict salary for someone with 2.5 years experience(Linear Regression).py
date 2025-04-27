a69de1493f67215936ad3ad9262f5b6a348ff1f7
Tanya Kumari <sonamtanya1996@gmail.com>
2025-04-27 21:29:36 +0530

    Linear Regression

---
+++ 03_Predict salary for someone with 2.5 years experience(Linear Regression).py
@@ -0,0 +1,14 @@
+"""
+    Q3.
+    Data:
+
+    x (Experience Years)	y (Salary in LPA)
+            1	                    4
+            2	                    5.5
+            3	                    7
+
+
+    Find the equation of line.
+    Predict salary for someone with 2.5 years experience.
+
+"""

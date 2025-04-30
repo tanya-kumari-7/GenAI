fad23b5969b77b4b38ad49cd8f2b78681cc04d22
Tanya Kumari <sonamtanya1996@gmail.com>
2025-04-30 17:08:58 +0530

    Linear Regression

---
+++ 11_ Predict price for 1.8K sq ft house.py
@@ -0,0 +1,13 @@
+"""
+🏠 Q5. House Size vs Price
+
+    Size (1000s sq ft) (x)	Price (Lakhs) (y)
+    1	50
+    1.5	65
+    2	80
+    2.5	95
+    📌 Task:
+
+    Find slope and intercept
+    Predict price for 1.8K sq ft house
+"""

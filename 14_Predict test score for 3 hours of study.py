11f79792801a5d1208f6c903873eb7bbed9ee716
Tanya Kumari <sonamtanya1996@gmail.com>
2025-05-02 17:28:54 +0530

    Linear Regression

--- 14_Predict test score for 3 hours of study.py
+++ 14_Predict test score for 3 hours of study.py
@@ -20,7 +20,9 @@ Ex2 = 1.5 **2 + 2.5 **2 + 3.5 **2 + 4.5 **2
 n = 4
 
 m = ( n * Exy - (Ex * Ey))/(n * Ex2 - (Ex **2))
+print("Slope (m):", m)
 
 b = ( Ey - m * Ex)/n
-
+print("Intercept (b):", b)
 y = m * 3 + b
+print("Predicted test score for 3 hours of study:", y)

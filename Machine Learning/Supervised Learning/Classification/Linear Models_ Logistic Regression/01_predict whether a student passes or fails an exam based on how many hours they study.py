b934e3b3f44571648d76d957a7836874527483c1
Tanya Kumari <sonamtanya1996@gmail.com>
2025-05-04 17:10:56 +0530

    logistic Regression

---
+++ Machine Learning/Supervised Learning/Classification/Linear Models_ Logistic Regression/01_predict whether a student passes or fails an exam based on how many hours they study.py
@@ -0,0 +1,34 @@
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+
+x = np.array([[1], [2], [3], [4], [5], [6], [7], [8], [9], [10]])
+y = np.array([0, 0, 0, 0, 0, 1, 1, 1, 1, 1])
+
+# Split the data into training and testing sets
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
+
+
+"""
+Explanation:
+
+70% of data is used to train the model
+30% is used to test how well it learned
+random_state ensures the split is the same every time you run it
+"""
+# Create a logistic regression model
+model = LogisticRegression()
+model.fit(x_train, y_train)
+
+# Make predictions on the test set
+y_pred = model.predict(x_test)
+print("Predicted labels: ", y_pred)
+
+# Calculate the accuracy of the model
+accuracy = accuracy_score(y_test, y_pred)
+print(accuracy)
+print("Actual labels: ", y_test)
+print("Accuracy:", accuracy_score(y_test, y_pred))

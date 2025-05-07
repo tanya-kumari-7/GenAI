338085a8ff319d542a1175ed3aaf8e09b45fe647
sonamtanya1996 <sonamtanya1996@gmail.com>
2025-05-07 22:37:35 +0530

    confusionMetrics

---
+++ Machine Learning/Supervised Learning/Classification/Confusion metrics/21_Medical Test Scenario.py
@@ -0,0 +1,21 @@
+Question 3: Medical Test Scenario
+A test checks if a person has a rare disease.
+Out of 1,000 people:
+
+50 actually have the disease
+
+The test correctly identifies 45 of them as positive
+
+It misses 5 (marked them as negative)
+
+It wrongly flags 30 healthy people as positive
+
+It correctly says 920 are negative
+
+Can you fill in the confusion matrix (TP, FP, TN, FN)?
+
+
+TP:  45
+FP: 30
+TN: 920
+FN: 5

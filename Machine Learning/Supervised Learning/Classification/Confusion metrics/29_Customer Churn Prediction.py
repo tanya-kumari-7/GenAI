3ba3f9552ac3ea74476a6c5315caa5b21c3fb4d2
sonamtanya1996 <sonamtanya1996@gmail.com>
2025-05-08 21:08:06 +0530

    confusionMetrics

---
+++ Machine Learning/Supervised Learning/Classification/Confusion metrics/29_Customer Churn Prediction.py
@@ -0,0 +1,24 @@
+🔹 Question 12: Customer Churn Prediction
+A telecom company predicts customer churn.
+
+Out of 5,000 customers:
+
+800 churned
+
+The model correctly predicted 650 of them
+
+It missed 150 churns
+
+It falsely predicted 200 loyal customers would churn
+
+The rest were correctly labeled as loyal
+
+Can you fill in the confusion matrix (TP, FP, TN, FN)?
+
+Q12.
+
+
+TP:  650
+FN: 150
+FP: 200
+TN: 4000

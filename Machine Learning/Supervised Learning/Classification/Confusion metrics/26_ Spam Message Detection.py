5d284196fbd758b7dfc7fe846a7980abe6a1af84
sonamtanya1996 <sonamtanya1996@gmail.com>
2025-05-08 20:59:43 +0530

    confusionMetrics

---
+++ Machine Learning/Supervised Learning/Classification/Confusion metrics/26_ Spam Message Detection.py
@@ -0,0 +1,24 @@
+🔹 Question 9: Spam Message Detection
+An SMS classifier predicts whether a message is spam or not.
+
+Out of 2,500 messages:
+
+400 were actually spam
+
+The model correctly flagged 350 as spam
+
+It missed 50 spam messages
+
+It incorrectly marked 100 non-spam messages as spam
+
+The rest were correctly marked as not spam
+
+Can you fill in the confusion matrix (TP, FP, TN, FN)?
+
+
+TP:  350
+FN: 50
+FP: 100
+TN: 2000
+
+

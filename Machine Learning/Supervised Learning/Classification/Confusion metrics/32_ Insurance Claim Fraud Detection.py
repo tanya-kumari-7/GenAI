fa77decafb10769b743b3a4a09506163fc93ca69
sonamtanya1996 <sonamtanya1996@gmail.com>
2025-05-08 21:15:12 +0530

    confusionMetrics

---
+++ Machine Learning/Supervised Learning/Classification/Confusion metrics/32_ Insurance Claim Fraud Detection.py
@@ -0,0 +1,23 @@
+Great! Here’s your next one:
+
+🔹 Question 15: Insurance Claim Fraud Detection
+An insurer runs a model to detect fraudulent claims.
+
+Out of 1,000 claims:
+
+180 were fraud
+
+160 were detected correctly
+
+20 were missed
+
+40 non-fraud claims were wrongly flagged as fraud
+
+The rest were labeled correctly as genuine
+
+Can you fill in the confusion matrix (TP, FP, TN, FN)?
+
+TP:  160
+FN: 20
+FP: 40
+TN: 780

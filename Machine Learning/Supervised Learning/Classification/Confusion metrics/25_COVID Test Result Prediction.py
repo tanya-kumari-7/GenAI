bed2e812095d9d82c6a3e5fbf79ce7f7c0adedb9
sonamtanya1996 <sonamtanya1996@gmail.com>
2025-05-08 20:54:47 +0530

    confusionMetrics

---
+++ Machine Learning/Supervised Learning/Classification/Confusion metrics/25_COVID Test Result Prediction.py
@@ -0,0 +1,23 @@
+Question 8: COVID Test Result Prediction
+A model is used to predict if someone has COVID based on symptoms.
+
+Out of 2,000 people tested:
+
+300 actually had COVID
+
+The model correctly identified 270 of them
+
+It missed 30 people who actually had COVID
+
+It falsely flagged 40 healthy people as COVID-positive
+
+The rest were correctly identified as healthy
+
+Can you fill in the confusion matrix (TP, FP, TN, FN)?
+
+Your turn!
+
+You said:
+TP:  270
+FP: 100
+TN: 800

61f5c7e2348537ca312a8913d18bd696dc2201cc
sonamtanya1996 <sonamtanya1996@gmail.com>
2025-05-08 20:45:52 +0530

    confusionMetrics

---
+++ Machine Learning/Supervised Learning/Classification/Confusion metrics/23_Credit Card Fraud Detection.py
@@ -0,0 +1,30 @@
+Question 6: Credit Card Fraud Detection
+A bank runs a model to detect fraudulent transactions.
+
+Out of 10,000 transactions:
+
+100 were actually fraud
+
+The model correctly detected 90 of them
+
+It missed 10 frauds
+
+It incorrectly flagged 50 legitimate transactions as fraud
+
+The rest were correctly identified as legitimate
+
+Can you fill in the confusion matrix (TP, FP, TN, FN)?
+
+Go ahead!
+
+
+
+
+
+
+
+You said:
+TP:  90
+FP: 50
+TN: 9850
+FN: 10

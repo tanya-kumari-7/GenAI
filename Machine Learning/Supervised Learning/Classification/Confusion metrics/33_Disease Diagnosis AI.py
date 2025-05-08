b5ef4e190a620b942fb71e45b0eb47011e60c934
sonamtanya1996 <sonamtanya1996@gmail.com>
2025-05-08 21:17:40 +0530

    confusionMetrics

---
+++ Machine Learning/Supervised Learning/Classification/Confusion metrics/33_Disease Diagnosis AI.py
@@ -0,0 +1,21 @@
+Great! Here's the final one for now:
+
+🔹 Question 16: Disease Diagnosis AI
+Out of 4,000 patients tested:
+
+500 actually had the disease
+
+The AI correctly identified 460 of them
+
+It missed 40
+
+It falsely diagnosed 70 healthy patients
+
+The rest were correctly diagnosed as healthy
+
+Can you fill in the confusion matrix (TP, FP, TN, FN)?
+
+TP:  460
+FN: 40
+FP: 70
+TN: 3430

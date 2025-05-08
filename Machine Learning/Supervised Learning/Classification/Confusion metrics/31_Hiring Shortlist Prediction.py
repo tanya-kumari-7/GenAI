99473d990ba0097acc0f73519fd008269742af8a
sonamtanya1996 <sonamtanya1996@gmail.com>
2025-05-08 21:13:14 +0530

    confusionMetrics

---
+++ Machine Learning/Supervised Learning/Classification/Confusion metrics/31_Hiring Shortlist Prediction.py
@@ -0,0 +1,26 @@
+🔹 Question 14: Hiring Shortlist Prediction
+A hiring AI shortlists candidates who will pass the final interview.
+
+Out of 800 candidates:
+
+120 passed the interview
+
+100 were correctly predicted
+
+20 were missed
+
+30 failed but were wrongly predicted to pass
+
+The rest were correctly predicted to fail
+
+Can you fill in the confusion matrix (TP, FP, TN, FN)?
+
+TP:  100
+FN: 20
+FP: 30
+TN: 650
+
+
+
+
+

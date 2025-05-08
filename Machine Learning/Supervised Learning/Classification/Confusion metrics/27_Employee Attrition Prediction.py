18ecce11d18933bb30e56d80bff7f66c1e52418f
sonamtanya1996 <sonamtanya1996@gmail.com>
2025-05-08 21:02:34 +0530

    confusionMetrics

---
+++ Machine Learning/Supervised Learning/Classification/Confusion metrics/27_Employee Attrition Prediction.py
@@ -0,0 +1,25 @@
+🔹 Question 10: Employee Attrition Prediction
+A company uses a model to predict which employees might leave.
+
+Out of 1,000 employees:
+
+200 actually left
+
+The model correctly predicted 160 would leave
+
+It missed 40 who did leave
+
+It wrongly predicted 60 would leave when they didn’t
+
+The rest were correctly predicted to stay
+
+Can you fill in the confusion matrix (TP, FP, TN, FN)?
+
+
+Q10.
+
+
+TP:  160
+FN: 40
+FP: 60
+TN: 740

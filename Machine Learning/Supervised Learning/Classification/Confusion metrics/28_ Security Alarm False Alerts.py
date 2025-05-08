e8ba6e0c914532d1c0bf181529368500593ebf60
sonamtanya1996 <sonamtanya1996@gmail.com>
2025-05-08 21:05:20 +0530

    confusionMetrics

---
+++ Machine Learning/Supervised Learning/Classification/Confusion metrics/28_ Security Alarm False Alerts.py
@@ -0,0 +1,24 @@
+🔹 Question 11: Security Alarm False Alerts
+A smart security system predicts intrusions.
+
+Out of 3,000 incidents:
+
+100 were actual break-ins
+
+The system correctly detected 95
+
+It missed 5
+
+It falsely raised alarms for 60 non-intrusions
+
+The rest were correctly labeled as safe
+
+Can you fill in the confusion matrix (TP, FP, TN, FN)?
+
+Q11.
+
+
+TP:  95
+FN: 5
+FP: 60
+TN: 2840

0aaef6bffb83e977f362d905ed4718ba1e601bde
sonamtanya1996 <sonamtanya1996@gmail.com>
2025-05-08 21:11:21 +0530

    confusionMetrics

---
+++ Machine Learning/Supervised Learning/Classification/Confusion metrics/30_Product Recommendation Accuracy.py
@@ -0,0 +1,22 @@
+🔹 Question 13: Product Recommendation Accuracy
+
+An e-commerce app recommends products. The model predicts whether a customer will click on the recommended product.
+
+Out of 3,500 recommendations:
+
+600 were actually clicked
+
+The model correctly predicted 500 of them
+
+It missed 100 clicks
+
+It wrongly predicted 150 would be clicked but weren’t
+
+The rest were correctly predicted as “no click”
+
+Can you fill in the confusion matrix (TP, FP, TN, FN)?
+
+TP:  500
+FN: 100
+FP: 150
+TN: 2750

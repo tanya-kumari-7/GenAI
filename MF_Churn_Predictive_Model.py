7506ff0728d045113dc31d64617834bb855b64ca
Tanya Kumari <87415938+tanya-kumari-7@users.noreply.github.com>
2025-08-13 11:50:48 +0530

    MF CHURN PREDICTIVE MODEL

---
+++ MF_Churn_Predictive_Model.py
@@ -0,0 +1,9 @@
+## Important Imports
+
+# import pandas as pd
+# from datetime import datetime 
+# import lightgbm as lgb
+# from sklearn.model_selection import train_test_split
+# from sklearn.metrics import classification_report, roc_auc_score
+# from sklearn.preprocessing import LabelEncode
+

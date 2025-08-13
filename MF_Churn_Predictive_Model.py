63e2ce7cb78841c5a5c1e1890cba7a0268a73022
Tanya Kumari <87415938+tanya-kumari-7@users.noreply.github.com>
2025-08-13 12:22:32 +0530

    MF CHURN PREDICTIVE MODEL

--- MF_Churn_Predictive_Model.py
+++ MF_Churn_Predictive_Model.py
@@ -1,9 +1,23 @@
-## Important Imports
+''' STEP:-1 Imports
+    STEP:-2 Prepare the Data
+    STEP:-3 Feature Engineering : creating, transforming, Encoding categorical variables,or selecting the variables (features)
+    STEP:-4 Do Exploratory Data Analysis
+    STEP:-5 Train-Test Split
+    STEP:-6 Choose & Train Models
+    STEP:-7 Evaluate Performance
+    STEP:-8 Deploy for Real-time Predictions 
+
+'''
 
 # import pandas as pd
 # from datetime import datetime 
 # import lightgbm as lgb
 # from sklearn.model_selection import train_test_split
 # from sklearn.metrics import classification_report, roc_auc_score
-# from sklearn.preprocessing import LabelEncode
+# from sklearn.preprocessing import LabelEncoder
 
+'''
+About My learning Till Here:
+This will be under supervised learning, as we are going to provide it with labeled data so it 
+can learn patterns and then make predictions for new datasets.
+'''
